9af8fc94ae86a3b11f5a7f37ce4c7f93d61fb637
OCTAVIO MARTINEZ <octandres2001.om9@gmail.com>
2025-11-11 09:47:09 -0300

    IMPLEMENTACION DE EXCEPCIONES PROPIAS PARA MANEJO DE COLAS

---
+++ src/Sudoku/EmptyQueueException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sudoku;
+
+/**
+ *
+ * @author OCTAVIO MARTINEZ
+ */
+
+public class EmptyQueueException extends RuntimeException {
+    public EmptyQueueException(String message) {
+        super(message);
+    }
+}
