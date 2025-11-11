1c17f04a879dc05e19c1c026088fd54582d83ee9
OCTAVIO MARTINEZ <octandres2001.om9@gmail.com>
2025-11-11 09:48:58 -0300

    IMPLEMENTACION DE EXCEPCIONES PROPIAS PARA MANEJO DE PILAS

---
+++ src/Sudoku/EmptyStackException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sudoku;
+
+/**
+ *
+ * @author OCTAVIO MARTINEZ
+ */
+
+
+public class EmptyStackException extends RuntimeException {
+    public EmptyStackException(String message) {
+        super(message);
+    }
+}
